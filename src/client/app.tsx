a30aeae54efd0136840a7fa4e7829baaad8f8430
jpig <isdolinskayaym@gmail.com>
2025-07-04 22:45:52 +0200

    added ssr to game page

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
-import NoSSR from './components/NoSSR';
 
 import GamePage from './pages/Game';
 
 function App() {
 	return (
-		<NoSSR>
-			<GamePage />
-		</NoSSR>
+		<GamePage />
 	);
 }
 
--- src/server/middlewares/pageTemplate.tsx
+++ src/server/middlewares/pageTemplate.tsx
@@ -1,9 +1,13 @@
 import type { Request, Response } from 'express';
 import * as React from 'react';
 import { renderToString } from 'react-dom/server';
+import { Provider } from 'react-redux';
+
+import store from '../store'
+
 import App from '../../client/app';
 
-const template = ({ title, app, scripts, styles }: any) => `
+const template = ({ title, app, scripts, styles }: any, { state }: any) => `
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -12,25 +16,40 @@ const template = ({ title, app, scripts, styles }: any) => `
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>${title}</title>
 		${Array.isArray(styles)
-			? styles.map(style => `<link href="${style}" rel="stylesheet" />`).join('')
-			: ''}
+		? styles.map(style => `<link href="${style}" rel="stylesheet" />`).join('')
+		: ''}
 	</head>
 	<body>
 		<div id="app">${app}</div>
+		        <script>
+          // WARNING: See the following for security issues around embedding JSON in HTML:
+          // https://redux.js.org/usage/server-rendering#security-considerations
+          window.__PRELOADED_STATE__ = ${JSON.stringify(state).replace(
+			/</g,
+			'\\u003c'
+		)}
+        </script>
 		${Array.isArray(scripts)
-			? scripts.map(script => `<script src="${script}" defer></script>`).join('')
-			: ''}
+		? scripts.map(script => `<script src="${script}" defer></script>`).join('')
+		: ''}
 	</body>
 </html>`;
 
 export default function middlewareServerSideRender(_req: Request, res: Response) {
-	const appMarkup = renderToString(React.createElement(App));
+	const appMarkup = renderToString(
+		<Provider store={store}>
+			<App />
+		</Provider>
+	);
+
+	const state = store.getState()
+
 	const html = template({
 		title: res.locals.title,
 		app: appMarkup,
 		scripts: res.locals.assets.scripts,
 		styles: res.locals.assets.styles,
-	});
+	}, { state });
 
 	let minifyRegExp = /(\n\t\t)/g;
 	let minifyReplace = "\n";
